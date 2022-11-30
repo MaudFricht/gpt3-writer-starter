1578c53d4d562f7c66b59832dc34daaf871dcd0f
Maud Frichement <maudfrichement@MacBook-Air-de-Maud.local>
2022-11-30 19:22:14 +0100

    Lafontaine Fable Generation - site

---
+++ pages/api/generate.js
@@ -0,0 +1,29 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix =
+`Écrit moi une fable pour enfant à la façon de Jean de La Fontaine. La fable doit contenir une morale inclue à la fin de l'histoire. Les deux personnages sont : 
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.5,
+    max_tokens: 700,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+
+  res.status(200).json({ output: basePromptOutput });
+
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
+
   return (
     <div className="root">
       <Head>
@@ -11,12 +41,43 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Ma fable comme La Fontaine</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Choisi deux ou trois animaux et obtient ta fable personnalisée.</h2>
           </div>
         </div>
+
+        <div className="prompt-container">
+          <textarea
+          className="prompt-box"
+          placeholder="Choisi tes personnages"
+          value={userInput}
+          onChange={onUserChangedText}/>
+        </div>
+
+        <div className="prompt-buttons">
+          <a className={isGenerating ? 'generate-button loading' : 'generate-button'}
+            onClick={callGenerateEndpoint}
+          >
+            <div className="generate">
+            {isGenerating ? <span className="loader"></span> : <p>Créer ma fable</p>}
+            </div>
+          </a>
+        </div>
+
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              {isGenerating ? <span className="loader"></span> : <p>{apiOutput}</p>}
+            </div>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a
